c06fd1e1f2ccb87099a7857e04907446494d03c6
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-07-24 12:15:31 -0300

    feat: TaskModel class

---
+++ Models/TaskModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
