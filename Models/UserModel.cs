d9debae04ee31ce5606dd62cf95cd0e480d54a83
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-07-24 12:13:49 -0300

    feat: UserModel class

---
+++ Models/UserModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskApi.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
