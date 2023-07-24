5cbdcaec35afd794098eaa989b9dcba962046686
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-07-24 12:22:13 -0300

    chore: Task CreatedAt property

--- Models/TaskModel.cs
+++ Models/TaskModel.cs
@@ -11,6 +11,8 @@ namespace TaskApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public StatusTask Status { get; set; }
 
     }
