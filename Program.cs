89c81097db3bb247c921105def975be7b24ac085
FelipeHeilmann <felipeheilmannm@gmail.com>
2023-07-24 13:58:31 -0300

    feat: Database conection and repository setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Data;
+using TaskApi.Repositories;
+using TaskApi.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<TaskDbContext>(
+        options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 var app = builder.Build();
 
