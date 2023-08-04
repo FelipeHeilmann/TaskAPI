ff62109de3b6615969de6afbeea062b6a1ab08e7
Felipe Heilmann Marques <69490701+FelipeHeilmann@users.noreply.github.com>
2023-08-03 22:44:18 -0300

    Delete Settings.cs

--- Settings.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.DataProtection;
-
-namespace TaskApi
-{
-    public class Settings
-    {
-        public static string Secret = "rdftcfvgbhnjbgtfvryhnbguvfytdc589126";
-    }
-}
